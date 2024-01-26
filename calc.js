5a7c96976d6b1baeb26ebb740af2d22547c3f747
Vinicius Coelho Gama <128941564+ViniciusCgama@users.noreply.github.com>
2024-01-26 14:35:10 -0300

    first commit

--- calc.js
+++
@@ -1,3 +0,0 @@
-    let nome = prompt("Digite o nome do meliante: ");
-
-    console.log("O meliante " + nome + " é perigo");
--- lista.js
+++ lista.js
@@ -1,5 +1,5 @@
 let n = [2, 4, 3, 1, 9, 8, 10, -1];
-let maiorNum, menorNum, i = 0;
+let maiorNum, menorNum, i = 1, temp;
 
 maiorNum = menorNum = n[0];
 
@@ -11,10 +11,20 @@ while (i < n.length) {
   if (n[i] < menorNum) {
     menorNum = n[i];
   }
-
   i++; 
+}
+
+ while ( i < n.length - 1 ) {
+    if (n[i] > n[i + 1]) {
+        const temp = n[i];
+        n[i] = n[i + 1];
+        n[i + 1] = temp;
+        }
+        else {
+            i++; 
+        }
 }
 
 console.log("O maior número é: " + maiorNum);
 console.log("O menor número é: " + menorNum);
-
+console.log("A lista organizada " + n);
