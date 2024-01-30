4362d5043f3178597eef7da090bc99a4f33dd2a0
Vinicius Coelho Gama <128941564+ViniciusCgama@users.noreply.github.com>
2024-01-30 13:28:20 -0300

    add while

--- lista.js
+++ lista.js
@@ -1,30 +1,42 @@
 let n = [2, 4, 3, 1, 9, 8, 10, -1];
-let maiorNum, menorNum, i = 1, temp;
+let maiorNum, menorNum, i = 0, j = n;
 
 maiorNum = menorNum = n[0];
 
-while (i < n.length) {
+while (true) {
   if (n[i] > maiorNum) {
     maiorNum = n[i];
-  }
-
-  if (n[i] < menorNum) {
+  } else if (n[i] < menorNum) {
     menorNum = n[i];
   }
-  i++; 
+  i++
+  
+  if (n[i] === undefined) {
+    break;
+  }
 }
 
- while ( i < n.length - 1 ) {
-    if (n[i] > n[i + 1]) {
-        const temp = n[i];
-        n[i] = n[i + 1];
-        n[i + 1] = temp;
-        }
-        else {
-            i++; 
-        }
+for (let i = 0; i < n.length; i++) {
+    for (let j = i + 1; j < n.length; j++) {
+      if (n[i] > n[j]) {
+        let temp = n[i];
+        n[i] = n[j];
+        n[j] = temp;
+      }
+    }
+}
+
+while (i<n.length) {
+  if (n[i] > n[i + 1]) {
+    const temp = n[i];
+    n[i] = n[i + 1];
+    n[i + 1] = temp;
+    i = 0;
+  }else{
+    i++
+  }
 }
 
 console.log("O maior número é: " + maiorNum);
 console.log("O menor número é: " + menorNum);
-console.log("A lista organizada " + n);
+console.log(n);
